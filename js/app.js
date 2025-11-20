26444d93d9963e732994af5096e322190ebe69f9
Marcielli Oliveira <marciellileticiaol@gmail.com>
2025-11-20 10:44:24 -0300

    Função cadastrar post e inserir na tabela

--- js/app.js
+++ js/app.js
@@ -47,17 +47,93 @@ function renderPostsTable(posts) {
 
 }
 
+//Faz POST na API
+async function createPost(title, body) {
+
+    try {
+
+        const response = await fetch(`${API}/posts`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title,
+                body,
+                userId: 1
+            })
+        });
+
+        const newPost = await response.json();
+        return newPost;
+
+    } catch(e) {
+        console.log("Erro ao criar o post: ", e);
+    }
+
+}
+
+function addPostToTable(post) {
+
+    const table = window.dataTableInstance;
+
+    table.row.add([
+        post.id,
+        post.title,
+        post.body,
+        `   <button class="btn-small blue edit-btn" data-id="${post.id}">Editar</button>
+            <button class="btn-small red delete-btn" data-id="${post.id}">Excluir</button>
+        `
+    ]).draw(false);
+
+}
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", async () => {
     const posts = await fetchPosts();
+
+    const modals = document.querySelectorAll(".modal");
+
     //console.log(posts);
     renderPostsTable(posts);
-
     
-    $('#posts-table').DataTable({
+    window.dataTableInstance = new DataTable('#posts-table', {
         info: true,
         paging: true,
         ordering: true,
         responsive: true
+    });
+
+    /*$('#posts-table').DataTable({
+        info: true,
+        paging: true,
+        ordering: true,
+        responsive: true
+    });*/
+
+    M.Modal.init(modals);
+
+    document.querySelector("#save-post").addEventListener("click", async () => {
+
+        const title = document.querySelector("#post-title").value;
+        const body = document.querySelector("#post-body").value;
+
+        if(!title || !body) {
+            M.toast({html:"Preencha todos os campos!"});
+            return;
+        }
+
+        const created = await createPost(title, body);
+        addPostToTable(created);
+
+        document.querySelector("#post-title").value = "";
+        document.querySelector("#post-body").value = "";
+
+        M.updateTextFields();
     });
 });
 
